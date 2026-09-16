62f4b900e7be27f1c04e86218d46586439238241
marxattacks <39135164+marxattacks@users.noreply.github.com>
2026-09-16 21:53:10 +0200

    Create nodes.js

---
+++ data/nodes.js
@@ -0,0 +1,26 @@
+id,name,tier,type,country,revenue,revenue_currency,pi_vw,components,includes,source,notes
+vw-ag,Volkswagen AG,0,Integrador,DE,324700,EUR,100,"VehÃ­culos completos (9M/aÃ±o)","Audi AG, Å koda, SEAT, Porsche, Lamborghini, Bentley, Cariad SE, Audi Hungaria, Kassel, VW Financial Services, Europcar",VW Group Annual Report 2024,"Integrador de sistemas. Concentra PI, plataformas MQB/SSP, gobernanza global"
+bosch,Robert Bosch,1,Generalista,DE,90500,EUR,,"InyecciÃ³n, ABS/ESC, software chasis","ESMC (10%, JV con TSMC/NXP/Infineon)",Bosch Annual Report 2024
+denso,Denso Corporation,1,Generalista,JP,47200,USD,13.5,"ElectrÃ³nica motor, HVAC, sensores, semiconductores",,SEC XBRL 20-F (2017-2022)
+magna,Magna International,1,Generalista,CA,43000,USD,,"CarrocerÃ­as, powertrain, battery covers, espejos",,VW Group Award 2025 (Product),"Puede ensamblar vehÃ­culos completos (Magna Steyr)"
+continental,Continental AG,1,Generalista,DE,39700,EUR,,"NeumÃ¡ticos, electrÃ³nica, software",,Continental Annual Report 2024,"EscisiÃ³n Aumovio 2025. Cotiza: CON.DE"
+zf,ZF Friedrichshafen,1,Generalista,DE,38000,EUR,,"Transmisiones, chasis, seguridad activa, frenos","TRW (2015), WABCO (2020), Lucas",Literatura analizada (documento base),"Privada. Incluye Lucas TRW absorbida"
+aisin,Aisin Corporation,1,Generalista,JP,35000,USD,,"Transmisiones, frenos, carrocerÃ­a",,Literatura sector automotriz,"Filial Toyota. Baja dependencia VW"
+forvia,Forvia / Faurecia,1,Generalista,FR,27000,EUR,,"Salpicaderos, control emisiones, iluminaciÃ³n","HELLA (2022)",Literatura analizada,"Cotiza. Incluye HELLA absorbida"
+valeo,Valeo,1,Generalista,FR,22000,EUR,,"IluminaciÃ³n, sistemas tÃ©rmicos, ADAS","FTE Automotive (frenos/embrague)",Literatura analizada,"Incluye FTE absorbida"
+lear,Lear Corporation,1,Generalista,US,22000,USD,,"Asientos, E-Systems",,Literatura analizada,"JIT/JIS directo a planta VW"
+motherson,Motherson Group,1,Generalista,IN,18000,USD,,"Bumpers, paneles puerta, cockpits, cableado, techos","SMP Deutschland, Yachiyo Industry (2024)","MarkLines ficha SMP Deutschland
+adient,Adient,1,Generalista,US,14500,USD,,"Asientos automotrices",,Literatura analizada,"Ex-Johnson Controls. Mayor fab. asientos"
+plastic-omnium,Plastic Omnium,1,Generalista,FR,10000,EUR,,"Exteriores plÃ¡sticos, sistemas energÃ­a",,Literatura analizada,"Cotiza"
+schaeffler,Schaeffler,2,Generalista,DE,16000,EUR,,"Rodamientos, transmisiones, chasis",,VW Group Award 2024 (Brands & Regions),"Generalista 2Âº orden. Cotiza SHA.DE"
+gestamp,Gestamp,2,Especialista,ES,13000,EUR,,"EstampaciÃ³n metÃ¡lica, carrocerÃ­a",,VW Group Award 2025 (People & Organization),"Cotiza BME:GEST"
+nemak,Nemak,2,Especialista,MX,5000,USD,13,"Bloques motor, culatas, battery housings (fundiciÃ³n aluminio)",,"MarkLines (3.0 TFSI V6)
+ronal,Ronal Group,1,Especialista,CH,2000,EUR,,"Llantas aleaciÃ³n",,"MarkLines
+infineon,Infineon Technologies,2,Especialista,DE,16000,EUR,,"Chips potencia, SiC, sensores",,VW Group Award
+nxp,NXP Semiconductors,2,Especialista,NL,12600,USD,,"MCUs automotrices",,Literatura analizada
+ti,Texas Instruments,2,Especialista,US,16000,USD,,"Analog/embedded semiconductors",,VW Group Award 2025 (Operational Excellence),"Cotiza TXN"
+stm,STMicroelectronics,2,Especialista,CH,13000,USD,,"MCUs, potencia",,Literatura analizada,"Revenue -23% en 2024. Cotiza STM"
+renesas,Renesas Electronics,2,Especialista,JP,12000,USD,,"MCUs/SoCs automotrices",,Literatura analizada,"7% auto semi share. Cotiza 6723.T"
+lg-es,LG Energy Solution,2,Especialista,KR,25000,USD,,"Celdas baterÃ­a para MEB",,VW Group Award 2024 (Partnership),"Gigafactories. Cotiza 373220.KS"
+gotion,Gotion High-Tech,2,Especialista,CN,5000,USD,,"Celdas baterÃ­a",,VW Group Award 2024 (Battery),"VW posee 26% participaciÃ³n directa"
+tsmc,TSMC,3,Especialista,TW,90000,USD,,"Obleas silicio, semiconductores (fundiciÃ³n)",,Literatura analizada,"Copropietario ESMC Dresden. CapEx >$30B/aÃ±o. Cotiza TSM"
